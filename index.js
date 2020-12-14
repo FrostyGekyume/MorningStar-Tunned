80e248602803594f674d67d742956ba2113a4249
FrostyGekyume <75546030+FrostyGekyume@users.noreply.github.com>
2020-12-14 20:28:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVIDOR] Servidor Ligado!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
+        
+        client.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
+            if (totalMem < 30) { 
+            	client.sendText(chat.id, `Infelizmente O Total De Membros É De ${totalMem}, O Mínimo Para A Permanência Do BOT É De 30 Membros`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            } else {
+                client.sendText(chat.groupMetadata.id, `Olá *${chat.contact.name}* ! Agradecemos Pela Preferência Em Utilizar Nosso BOT!! Para A Lista De Comandos Dê !menu`)
+            }
+        }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Não Sou Seu Amigo Para Ligar Para Mim! Sou Um BOT! Ligação = Block')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+create(options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
